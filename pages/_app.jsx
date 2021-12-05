26805a98da405a61cf3c76d12a4c0ec01e8f7d9a
Dan <48622577+danjlewis@users.noreply.github.com>
2021-12-05 22:44:00 +0000

    2021-12-05 22:44:00 (UTC+0000)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import '../styles/global.css';
 
 export default function App({Component, pageProps}) {
-    return <Component id='root' {...pageProps} />;
+    return <Component {...pageProps} />;
 }
